f1e79895bf77d901add98dc0b9dccb09a9ec796b
davgo0103 <davvgo@gmail.com>
2021-10-05 10:00:53 +0800

    修改視窗，加入控制項

---
+++ WpfApp1/Drink.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp1
+{
+    class Drink
+    {
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public int Price { get; set; }
+
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,11 +23,53 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Drink> drinks = new List<Drink>();
+
+            //新增飲料品項
+            drinks.Add(new Drink() { Name = "咖啡",Size="大杯",Price = 60 });
+            drinks.Add(new Drink() { Name = "咖啡",Size="中杯",Price = 50 });
+            drinks.Add(new Drink() { Name = "紅茶",Size="大杯",Price = 40 });
+            drinks.Add(new Drink() { Name = "紅茶",Size="中杯",Price = 30 });
+            drinks.Add(new Drink() { Name = "綠茶",Size="大杯",Price = 40 });
+            drinks.Add(new Drink() { Name = "綠茶",Size="中杯",Price = 30 });
+            drinks.Add(new Drink() { Name = "礦泉水",Size="大杯",Price = 20 });
+            drinks.Add(new Drink() { Name = "礦泉水",Size="小杯",Price = 10 });
+
+            //顯示所有飲料品項
+            DisplayDrink(drinks);
+
+        }
+
+        private void DisplayDrink(List<Drink> myDrink)
+        {
+            foreach (Drink d in myDrink)
+            {
+                CheckBox cb = new CheckBox();
+                StackPanel sp = new StackPanel();
+                Slider sl = new Slider();
+                Label lb = new Label();
+
+                sp.Orientation = Orientation.Horizontal;
+                cb.Content = d.Name + d.Size + d.Price;
+
+                sl.Width = 150;
+                lb.Width = 30;
+                lb.Content = "0";
+
+                sp.Children.Add(cb);
+                sp.Children.Add(sl);
+                sp.Children.Add(lb);
+                DrinkMenu.Children.Add(sp);
+
+            }
+
+
         }
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-            label1.Content = "Hello WPF !";
+            
         }
     }
 }
